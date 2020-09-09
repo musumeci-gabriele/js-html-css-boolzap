b0f28946ae567b15c2f04645e29c3a0e7e47fbbb
Gabriele-Musumeci <musumeci.gabriele@hotmail.com>
2020-09-09 19:06:24 +0200

    time

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
 $(document).ready(function(){
 
+  // // GENERAZIONE MESSAGGIO AUTOMATICO
+  function sendMassage(){
+    var inputText = $("#input-massage").val();
+
+    if (inputText != "") {
+      var templateMassage = $(".template .massage-row").clone();
+      templateMassage.find("massage-text").text(inputText);
+      templateMassage.find("massage-time").text("time");
+      templateMassage.addClass("sent");
+
+      $(".chat").append(templateMassage);
+      $("#input-message").val("");
+    }
+  }
+
+  // EVENTO CLICK SUL TASTO MIC
   $(".send-massage").click(
     function(){
       sendMassage();
     }
   );
-  $("#input-message").keyup(
+
+  // EVENTO PRESSIONE TASTO INVIO
+  $("#input-msg").keyup(
     function(event) {
       if (event.which ==13){
         sendMassage();
@@ -13,23 +31,22 @@ $(document).ready(function(){
     }
   );
 
-
-});
-function sendMassage(){
-  var inputText = $(""#input-massage").val();
-
-  var date = new Date ();
-  var hours = date.getHours();
-  var minutes = date.getMinutes();
-  var time = hours +":"+ minutes;
-
-  if (inputText != "") {
-    var templateMassage = $(".template .massage-row").clone();
-    templateMassage.find("massage-text").text(inputText);
-    templateMassage.find("massage-time").text("time");
-    templateMassage.addClass("sent");
-
-    $(".chat").append(templateMassage);
-    $("#input-message").val("");
+  //  OROLOGIO
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
   }
-}
+
+  var clock = document.getElementById('clock');
+
+  function time() {
+    var d = new Date();
+    var m = d.getMinutes();
+    m = checkTime(m);
+    var h = d.getHours();
+    clock.textContent = (h + ":" + m);
+  }
+
+  setInterval(time, 1000);
+  console.log(clock);
+});
